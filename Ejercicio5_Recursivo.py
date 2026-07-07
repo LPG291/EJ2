9b402ce486642f8523cc336d698866f3f106b486
LPG291 <luis.chavez@becarios.silent4business.com>
2026-07-07 17:04:10 -0500

    Delete Ejercicio5_Recursivo.py

--- Ejercicio5_Recursivo.py
+++
@@ -1,8 +0,0 @@
-def fibonacci(n):
-    if n <= 1:
-        return n
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
-
-resultado = fibonacci(4)
-print(f"El término 6 de la serie de Fibonacci es: {resultado}")
